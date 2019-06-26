811ad56e7d7f44b4fe258c611cf0360ad5a32d0f
Victor <victor.stf@gmail.com>
2019-06-26 22:13:16 +0100

    fix client

--- soapClient.js
+++ soapClient.js
@@ -1,12 +1,18 @@
 var soap = require('soap');
-  var url = 'http://localhost:8001/stockquote?wsdl';
+var config = require("./config.json")
+
+function testClient(server, port) {
+    var url = `http://${server}:${port}/stockquote?wsdl`;
   
-  var args = {TradePriceRequest: {
-    tickerSymbol: 'value'
-  }};
-  
-  soap.createClient(url, function(err, client) {
-    client.GetLastTradePrice(args, function(err, result) {
-        console.log(result);
+    var args = {TradePriceRequest: {
+      tickerSymbol: 'value'
+    }};
+    
+    soap.createClient(url, function(err, client) {
+      client.GetLastTradePrice(args, function(err, result) {
+          console.log(result);
+      });
     });
-});
+}
+
+testClient(config.stockServer, config.stockServerPort)
--- soapServer.js
+++ soapServer.js
@@ -2,6 +2,7 @@ var http = require("http")
 var soap = require("soap")
 var express = require("express")
 var bodyParser = require('body-parser')
+var config = require("./config.json")
 
 var myService = {
     StockQuoteService: {
@@ -21,25 +22,11 @@ var myService = {
     }
 };
 
-var xml = require('fs').readFileSync('myService.wsdl', 'utf8');
+var xml = require('fs').readFileSync('stockquote.wsdl', 'utf8');
 
-//http server example
-var server = http.createServer(function(request,response) {
-    response.end('404: Not Found: ' + request.url);
-});
-
-// server.listen(8000);
-// soap.listen(server, '/wsdl', myService, xml, function(){
-//   console.log('server initialized');
-// });
-
-//express server example
 var app = express();
-//body parser middleware are supported (optional)
 app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
-app.listen(8001, function(){
-    //Note: /wsdl route will be handled by soap module
-    //and all other routes & middleware will continue to work
+app.listen(config.stockServerPort, function(){
     soap.listen(app, '/stockquote', myService, xml, function(){
       console.log('server initialized');
     });
